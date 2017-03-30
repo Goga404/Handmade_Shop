c1a50cbb4437496cecc701d9059321122326b97f
Goga404 <justanothergoga@gmail.com>
2017-03-30 21:17:31 +0300

    Load_Site

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+﻿function main() {
+    $('.button').hide(); /* added a CSS property of display: none */
+    $('#sale').fadeIn(2000);
+    $('#kids').fadeIn(3000);
+    $('#her').fadeIn(4000);
+}
+
+$(document).ready(main); /* jQuery built in function to check if the page is ready before it will run our code. 
+                         * 
+                         Функция main будет выполнена только после прогрузки документа*/
